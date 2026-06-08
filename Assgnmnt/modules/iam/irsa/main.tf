d0834027a6f70e6b3c16581dd6e75ad6bb737082
lchahal <123313031+lchahalx@users.noreply.github.com>
2026-06-08 11:21:56 -0400

    Create main.tf

---
+++ Assgnmnt/modules/iam/irsa/main.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "irsa" {
+  name = "${var.project}-${var.role_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = var.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${var.oidc_provider_url}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account_name}"
+          "${var.oidc_provider_url}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_policy" "irsa" {
+  name   = "${var.project}-${var.role_name}-policy"
+  policy = var.policy_document
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "irsa" {
+  role       = aws_iam_role.irsa.name
+  policy_arn = aws_iam_policy.irsa.arn
+}
